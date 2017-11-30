2793fbc64141b92f4a0c37e6695a84659cc8b817
Christian Schmidt <christian@i10-221.informatik.rwth-aachen.de>
2017-11-30 17:28:55 +0100

    Added Websites, Images, folders and thesis template

---
+++ Code/Arduino Code/_4er_motor_test/_4er_motor_test.ino
@@ -0,0 +1,19 @@
+
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(10, 255);
+  digitalWrite(11, 255);
+  digitalWrite(12, 255);
+  digitalWrite(13, 255);
+
+}
+
---
+++ Literature/Websites/POSYDON Is Like GPS, But for Underwater Submarine Drones - The Atlantic_files/advertisement.js
@@ -0,0 +1,1 @@
+window.ads_enabled = true;
